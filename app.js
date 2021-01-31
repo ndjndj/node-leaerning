6236de546241896ef24a45389ee55e7bd588ba7b
ndjndj <teq.ndj@gmail.com>
2021-01-31 20:33:54 +0900

    app.js p80

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const http = require('http');
 const fs = require('fs');
 
-const server = http.createServer(
-    (request, response) => {
-        fs.readFile(
-              './index.html'
-            , 'UTF-8'
-            , (error, data) => {
-                response.writeHead(200, {'Content-Type': 'text.html'});
-                response.write(data);
-                response.end();
-            }
-        );
-    }
-);
+// main
+const server = http.createServer(getFromClient);
 
 server.listen(3000);
 console.log('Server start');
+
+//createServer
+function getFromClient(req, res) {
+    request = req;
+    response = res;
+    fs.readFile(
+          './index.html'
+        , 'UTF-8'
+        , (error, data) => {
+            response.writeHead(200, {'Content-Type': 'text.html'});
+            response.write(data);
+            response.end();
+        }
+    );
+}
