34bc3fbcd210a1fb43c319e7ed970d571a304527
ndjndj <teq.ndj@gmail.com>
2021-01-31 20:08:05 +0900

    p70

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require('http');
 
-let server = http.createServer(
+const server = http.createServer(
     (request, response) => {
-        response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
+        response.setHeader('Content-Type', 'text/html');
+        response.write('<!DOCTYPE html><html lang="ja">');
+        response.write('<head><meta charset="utf-8">');
+        response.write('<title>Hello</title></head>');
+        response.write('<body><h1>Hello Node.js!!!</h1>');
+        response.write('<p>これは、Node.js のサンプルページです。</p>', 'utf8');
+        response.write('<p>This is Node.js sample page.</p>');
+        response.write('</body></html>');
+        response.end();
     }
 );
 
 server.listen(3000);
+console.log('Server start');
