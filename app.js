d131941709be1ef0ef3951bf790a932d34ffe7d7
ndjndj <teq.ndj@gmail.com>
2021-01-31 19:22:13 +0900

    app.js p67

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 let server = http.createServer(
     (request, response) => {
-        response.end('Hello World!');
+        response.end('<html><body><h1>Hello</h1><p>Welcome to Node.js</p></body></html>');
     }
 );
 
