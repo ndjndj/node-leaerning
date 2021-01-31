8562522240b31410458cb49e121a3c5e7eb82229
ndjndj <teq.ndj@gmail.com>
2021-01-31 23:48:20 +0900

    app.js p100

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const fs = require('fs');
 const ejs = require('ejs');
+const url = require('url');
 
 const indexPage = fs.readFileSync('./index.ejs', 'utf8');
+const styleCSS = fs.readFileSync('./style.css', 'utf8');
 // main
 const server = http.createServer(getFromClient);
 
@@ -11,14 +13,29 @@ console.log('Server start');
 
 //createServer
 function getFromClient(request, response) {
-    const content = ejs.render(
-          indexPage
-        , {
-              title: 'Indexページ'
-            , content: 'これはテンプレートを使ったサンプルページです。'
-        }
-    );
-    response.writeHead(200, {'Content-Type': 'text.html'});
-    response.write(content);
-    response.end();
+    const urlParts = url.parse(request.url);
+
+    switch (urlParts.pathname) {
+        case '/':
+            const content = ejs.render(
+                indexPage
+              , {
+                    title: 'Indexページ'
+                  , content: 'これはテンプレートを使ったサンプルページです。'
+              }
+            );
+            response.writeHead(200, {'Content-Type': 'text.html'});
+            response.write(content);
+            response.end();
+            break;
+        case './style.css':
+            response.writeHead(200, {'Content-Type': 'text.html'});
+            response.write(styleCSS);
+            response.end();
+            break;
+        default:
+            response.writeHead(200, {'Content-Type': 'text.html'});
+            response.end('no page...');
+            break;
+    }
 }
