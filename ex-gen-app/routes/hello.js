3f7156c416db1880e4730af4783d03dec737ffc8
ndjndj <teq.ndj@gmail.com>
2021-02-03 01:29:59 +0900

    hello.js p222

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -1,32 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const http = require('https');
+const parseString = require('xml2js').parseString;
 
 router.get(
       '/'
     , (req, res, next) => {
-        var msg = '※何か書いて送信してください';
-        if (req.session.message !== undefined) {
-            msg = `Last Message: ${String(req.session.message)}`;
+        const opt = {
+              host: 'news.google.com'
+            , port: 443
+            , path: '/rss?hl=ja&ie=UTF-8&oe=UTF-8&gl=JP&ceid=JP:ja'
         }
-        var data = {
-              title: 'Hello!'
-            , content: msg
-        };
-        res.render('hello', data);
-    }
-);
-
-router.post(
-      '/post'
-    , (req, res, next) => {
-        var msg =  req.body['message'];
-        req.session.message = msg;
-        console.log(req.session.message);
-        var data = {
-              title: 'Hello!'
-            , content: `Last Message: ${String(req.session.message)}`
-        }
-        res.render('hello', data);
+        http.get(
+              opt
+            , (res2) => {
+                var body = '';
+                res2.on(
+                      'data'
+                    , (data) => {
+                        body += data;
+                    }
+                );
+                res2.on(
+                      'end'
+                    , () => {
+                        parseString(
+                              body.trim()
+                            , (err, result) => {
+                                console.log(result);
+                                var data = {
+                                      title: 'Google News'
+                                    , content: result.rss.channel[0].item
+                                };
+                                res.render('hello', data);
+                            }
+                        );
+                    }
+                );
+            }
+        );
     }
 );
 
