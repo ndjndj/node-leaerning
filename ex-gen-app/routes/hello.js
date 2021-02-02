48d37d0efed733af4f552b6abb89924f9401ea18
ndjndj <teq.ndj@gmail.com>
2021-02-02 23:25:01 +0900

    hello.js p211

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -4,14 +4,23 @@ const router = express.Router();
 router.get(
       '/'
     , (req, res, next) => {
-        var name = req.query.name;
-        var mail = req.query.mail;
         var data = {
               title: 'Hello!'
-            , content: `あなたの名前は${name}。<br>メールアドレスは${mail}です。`
+            , content: '※何か書いて送信してください。'
         };
         res.render('hello', data);
     }
 );
 
-module.exports = router
+router.post(
+      '/post'
+    , (req, res, next) => {
+        var data = {
+              title: 'Hello!'
+            , content: `あなたは「${msg}」と送信しました。`
+        }
+        res.render('hello', data);
+    }
+);
+
+module.exports = router;
