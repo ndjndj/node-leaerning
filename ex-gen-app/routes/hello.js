c6932fabac0cabbd7f62d04c0b325cda523496cc
ndjndj <teq.ndj@gmail.com>
2021-02-04 02:31:54 +0900

    hello.js

--- ex-gen-app/routes/hello.js
+++ ex-gen-app/routes/hello.js
@@ -64,4 +64,31 @@ router.post(
   }
 );
 
+router.get(
+    '/show'
+  , (req, res, next) => {
+      const id = req.query.id;
+      db.serialize(
+          () => {
+              const q = 'select * from mydata where id = ?';
+              db.get(
+                    q
+                  , [id]
+                  , (err, row) => {
+                      if(!err) {
+                          var data = {
+                                title: 'Hello/show'
+                              , content: `id = ${id} のレコード`
+                              , mydata: row
+                          }
+                      }
+                      res.render('hello/show', data);
+                  }
+              );
+          }
+      );
+  }
+);
+
+
 module.exports = router;
