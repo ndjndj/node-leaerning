b9cf0ff7f406a4c15e630cdfa1b2d30955b26b7e
ndjndj <teq.ndj@gmail.com>
2021-02-09 23:58:35 +0900

    router post login with users.js

--- message-board/routes/users.js
+++ message-board/routes/users.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const db = require('../../ex-gen-app/models');
 var router = express.Router();
 
 /* GET users listing. */
@@ -14,6 +15,36 @@ router.get(
       , content: '名前とパスワードを入力してください'
     }
     res.render('users/login', data);
+  }
+);
+
+router.post(
+    '/login'
+  , (req, res, next) => {
+    db.User.findOne({
+      where: {
+          name: req.body.name
+        , pass: req.body.pass
+      }
+    })
+    .then(
+      usr => {
+        if (usr != null) {
+          req.session.login = usr;
+          let back = req.session.back;
+          if (back == null) {
+            back = '/';
+          }
+          res.redirect(back);
+        } else {
+          var data  = {
+              title: 'Users/Login'
+            , content: '名前かパスワードに問題があります。再度入力してください'
+          }
+          res.render('users/login', data);
+        }
+      }
+    )
   }
 );
 
