4565b1a2dfb5ba77286889cfc997f9ca74996e63
ndjndj <teq.ndj@gmail.com>
2021-02-09 23:54:07 +0900

    router get user.js login

--- message-board/routes/users.js
+++ message-board/routes/users.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get(
+    '/login'
+  , (req, res, next) => {
+    var data = {
+        title: 'Users/Login'
+      , content: '名前とパスワードを入力してください'
+    }
+    res.render('users/login', data);
+  }
+);
+
 module.exports = router;
