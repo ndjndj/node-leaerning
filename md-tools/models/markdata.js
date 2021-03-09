585535bf8a83e12c88933f6676f74a1f9ddaafc1
ndjndj <teq.ndj@gmail.com>
2021-03-10 00:31:59 +0900

    markdata models

--- md-tools/models/markdata.js
+++ md-tools/models/markdata.js
@@ -1,25 +1,37 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Markdata extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  const Markdata = sequelize.define(
+      'Markdata'
+    , {
+        userId: {
+            type: DataTypes.INTEGER
+          , validate: {
+            notEmpty: {
+              msg: '利用者は必須です。'
+            }
+          }
+        }
+      , title: {
+            type: DataTypes.STRING
+          , validate: {
+            notEmpty: {
+              msg: 'タイトルは必須です。'
+            }
+          }
+        }
+      , content: {
+            type: DataTypes.TEXT
+          , validate: {
+            notEmpty: {
+              msg: 'コンテンツは必須です。'
+            }
+          }
+        }
     }
+    , {}
+  );
+  Markdata.associate = function(models) {
+    Markdata.belongsTo(models.User);
   };
-  Markdata.init({
-    userId: DataTypes.INTEGER,
-    title: DataTypes.STRING,
-    content: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Markdata',
-  });
   return Markdata;
-};
+}
